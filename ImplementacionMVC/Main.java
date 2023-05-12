ea2334fcb0bdb8acc8081fb99a332839761bbc15
Dann013 <125938760+Dann013@users.noreply.github.com>
2023-05-11 23:38:14 -0600

    Add files via upload

---
+++ ImplementacionMVC/Main.java
@@ -0,0 +1,12 @@
+
+package ImplementacionMVC;
+
+/**
+ *
+ * @author Gateway
+ */
+public class Main {
+    public static void main(String[] args) {
+        Ventana ventana = new Ventana("Combobox MVC");
+    }
+}

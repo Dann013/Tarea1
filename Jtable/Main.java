166d6cc45ab9fdbc41d989d7ea1564e5fa5f1b27
Dann013 <125938760+Dann013@users.noreply.github.com>
2023-05-14 23:57:13 -0600

    Add files via upload

---
+++ Jtable/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Jtable;
+
+/**
+ *
+ * @author Gateway
+ */
+public class Main {
+    public static void main(String args[]) {
+        Ventana ventana = new Ventana("Ejemplo Ventana MVS");
+    }
+}
+}
